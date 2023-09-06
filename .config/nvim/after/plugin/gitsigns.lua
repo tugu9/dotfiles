03de67112780d9587d6dd9691f6bb592d99808bb
tugu <tugu@tugus-MBP.home>
2023-09-06 01:26:37 -0700

    gitsign split view

--- .config/nvim/after/plugin/gitsigns.lua
+++ .config/nvim/after/plugin/gitsigns.lua
@@ -1,6 +1,9 @@
 require('gitsigns').setup()
+local gitsigns = require('gitsigns')
 
 vim.cmd ":highlight GitSignsAdd guibg=NONE"
 vim.cmd ":highlight GitSignsChange guibg=NONE"
 vim.cmd ":highlight GitSignsDelete guibg=NONE"
 
+vim.keymap.set('n', '<leader>gd', function()
+    gitsigns.diffthis() end, { remap = true })
