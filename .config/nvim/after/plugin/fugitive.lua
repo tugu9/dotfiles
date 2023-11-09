b8997bb2988c2e84c94c42195819ad7045d09e72
tugu <tugu@nomat.local>
2023-11-09 12:48:55 -0800

    leader gg for Git difftool

--- .config/nvim/after/plugin/fugitive.lua
+++ .config/nvim/after/plugin/fugitive.lua
@@ -1,5 +1,6 @@
 vim.keymap.set("n", "<leader>gs", ':Git<CR><C-w>15_');
 vim.keymap.set('n', '<leader>gb', ':Git blame<CR>');
 vim.keymap.set('n', '<leader>gd', ':Gdiffsplit!<CR>')
+vim.keymap.set('n', '<leader>gg', ':Git difftool<CR>')
 vim.keymap.set('n', '<leader>gm', ':Git mergetool<CR>')
 
